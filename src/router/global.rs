72c4f88e0304097ced392e5d2d0be6bb3bb7b7fe
Ethan O'Brien <77750390+ethanaobrien@users.noreply.github.com>
2024-03-30 11:10:56 -0500

    Add more logs

--- src/router/global.rs
+++ src/router/global.rs
@@ -9,7 +9,7 @@ use base64::{Engine as _, engine::general_purpose};
 
 //different between ios and android?
 pub const ASSET_VERSION: &str = "13177023d4b7ad41ff52af4cefba5c55";
-pub const ASSET_HASH:    &str = "9fbfeda43a5cbf744ef23c06c22170aa";
+pub const ASSET_HASH:    &str = "6a6f3be1da2c3734386a1832e251451a";
 
 pub const ASSET_VERSION_JP:       &str = "4c921d2443335e574a82e04ec9ea243c";
 pub const ASSET_HASH_ANDROID_JP:  &str = "67f8f261c16b3cca63e520a25aad6c1c";
