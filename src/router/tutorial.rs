b583c82364c3f46f9b2f6b074162e67fda657786
Ethan O'Brien <77750390+ethanaobrien@users.noreply.github.com>
2024-04-02 10:43:36 -0500

    Set stamina to 100 during tutorial

--- src/router/tutorial.rs
+++ src/router/tutorial.rs
@@ -11,6 +11,8 @@ pub fn tutorial(req: HttpRequest, body: String) -> HttpResponse {
     let mut user = userdata::get_acc(&key);
     
     user["tutorial_step"] = body["step"].clone();
+    user["stamina"]["stamina"] = (100).into();
+    user["stamina"]["last_updated_time"] = global::timestamp().into();
     
     userdata::save_acc(&key, user);
     
