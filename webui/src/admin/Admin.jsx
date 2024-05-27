e9191150f941959a32cc8eae852d71ce436c843f
Ethan O'Brien <ethan.a.obrien@gmail.com>
2024-05-27 15:48:44 -0500

    Forgot to upload these files

---
+++ webui/src/admin/Admin.jsx
@@ -0,0 +1,46 @@
+import { useState, useParams, useEffect } from 'react'
+import './Admin.css'
+import Request from '../Request.jsx'
+
+function Admin() {
+    const [imp, setimp] = useState();
+    const [exp, setexp] = useState();
+    const ids = [setimp, setexp];
+    
+    const handleSubmit = async (id, event) => {
+        ids[id](event.target.checked);
+        await Request(
+            "/api/webui/admin",
+            {
+                import: !!imp,
+                export: !!exp
+            }
+        );
+    };
+    
+    if (imp === undefined) {
+        (async () => {
+            let resp = await Request("/api/webui/admin");
+            if (resp.result !== "OK") {
+                window.location.href = "/?message=" + encodeURIComponent(resp.message);
+                return;
+            }
+            setimp(resp.data.import);
+            setexp(resp.data.export);
+        })();
+    }
+  
+    return (
+        <div id="home">
+            <h1>Admin</h1>
+            <div>
+            <input type="checkbox" id="import" name="import" checked={!!imp} onClick={(i)=>handleSubmit(0, i)} />
+            <label for="import">Allow account imports</label><br/><br/>
+            <input type="checkbox" id="exp" name="exp" checked={!!exp} onClick={(i)=>handleSubmit(1, i)} />
+            <label for="exp">Allow account exports</label>
+            </div>
+        </div>
+    );
+}
+
+export default Admin;
