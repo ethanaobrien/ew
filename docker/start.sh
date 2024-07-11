1bdf0d5811e7466494fda7adb44b9a8757fb63b1
Ethan O'Brien <ethan.a.obrien@gmail.com>
2024-07-11 09:15:37 -0500

    Add updated docker files

--- docker/start.sh
+++ docker/start.sh
@@ -7,4 +7,4 @@ npps4="${NPPS4_ADDRESS:-http://127.0.0.1:51376}"
 
 https=$([ "$HTTPS" = "true" ] && echo "--https" || echo "")
 
-/root/ew/ew --path $directory --port $port --npps4 $npps4 $https
+/root/ew/ew --path $directory --port $port --npps4 $npps4 $https --global-android "$ANDROID_GLOBAL"  --japan-android "$ANDROID_JAPAN"  --global-ios "$IOS_GLOBAL"  --japan-ios "$IOS_JAPAN" --assets-url "$ASSET_URL"
