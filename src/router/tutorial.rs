21849b23222d43ecdc68ca0d012027606c3205ba
Ethan O'Brien <ethan.a.obrien@gmail.com>
2024-10-18 14:54:39 -0500

    Ignore /api/tutorial requests when tutorial is already completed

--- src/router/tutorial.rs
+++ src/router/tutorial.rs
@@ -8,12 +8,13 @@ pub fn tutorial(req: HttpRequest, body: String) -> Option<JsonValue> {
     let key = global::get_login(req.headers(), &body);
     let body = json::parse(&encryption::decrypt_packet(&body).unwrap()).unwrap();
     let mut user = userdata::get_acc(&key);
-    
-    user["tutorial_step"] = body["step"].clone();
-    user["stamina"]["stamina"] = (100).into();
-    user["stamina"]["last_updated_time"] = global::timestamp().into();
-    
-    userdata::save_acc(&key, user);
+
+    if user["tutorial_step"].as_i32().unwrap() < 130 {
+        user["tutorial_step"] = body["step"].clone();
+        user["stamina"]["stamina"] = (100).into();
+        user["stamina"]["last_updated_time"] = global::timestamp().into();
+        userdata::save_acc(&key, user);
+    }
     
     Some(array![])
 }
