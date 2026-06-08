5f6dd7cbc087f737472597aa8d479a84bd706ecf
Ethan O'Brien <ethan.a.obrien@gmail.com>
2026-06-08 13:57:38 -0500

    Add temporary logging

--- src/router/global.rs
+++ src/router/global.rs
@@ -99,6 +99,7 @@ pub fn get_player_region(asset_version: &str) -> Option<String> {
 }
 
 pub fn parse_platform(header: &str) -> &str {
+    println!("{header}");
     let platform = header.split_whitespace().next().unwrap_or("").to_lowercase();
     match platform.as_str() {
         "android" => "Android",
